8c86f606e558921fb7ff0426479fe28aaa8ab687
Степан Прокопьев <dr.aibolitarise@mail.ru>
2026-05-18 23:19:48 +0900

    фича: добавлен URL для установки часового пояса
    - /set-timezone/ - эндпоинт для обновления timezone
    - ащищён JWT авторизацией
    - PATCH метод для частичного обновления

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
     RegisterView, LoginView, ProfileView,
-    SetTelegramChatIdView, SetTelegramNotificationsView, CurrentUserView
+    SetTelegramChatIdView, SetTelegramNotificationsView,
+    SetTimezoneView, CurrentUserView
 )
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -13,4 +14,5 @@ urlpatterns = [
     path('current/', CurrentUserView.as_view(), name='current_user'),
     path('set-telegram-id/', SetTelegramChatIdView.as_view(), name='set-telegram-id'),
     path('set-telegram-notifications/', SetTelegramNotificationsView.as_view(), name='set-telegram-notifications'),
+    path('set-timezone/', SetTimezoneView.as_view(), name='set-timezone'),
 ]
